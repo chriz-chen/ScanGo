ec080ebb04875cfc2bcf793bc8c5f8f621fd791c
chris <chris@localhost>
2024-01-08 16:29:04 +0800

    update

--- ScanGo/src/main/java/com/example/bean/LoginUser.java
+++ ScanGo/src/main/java/com/example/bean/LoginUser.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 public class LoginUser {
 
 	@NotEmpty(message = "請輸入帳號")
-	private String username;
+	private String username; 
 
 	@NotEmpty(message = "請輸入密碼")
 	private String password;
