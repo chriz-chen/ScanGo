2693d544d5a5aac837ceac97162c2ba94de86541
stone <stone19970207@gmail.com>
2024-01-17 15:43:07 +0800

    update

--- ScanGo/src/main/java/com/example/controller/CartController.java
+++ ScanGo/src/main/java/com/example/controller/CartController.java
@@ -1,6 +1,8 @@
 package com.example.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -10,15 +12,16 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.dao.CartDAO;
 import com.example.entity.Cart;
-import com.example.entity.Product;
 import com.example.entity.User;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 @Controller
@@ -46,19 +49,39 @@ public class CartController {
 	}
 	
 	@GetMapping("/drawer_cart")
+	@ResponseBody
 	public String drawer_cartPage(HttpSession session, Model model) {
-		//  先找到 user 登入者
-		User user = (User)session.getAttribute("user");
-		
-		Integer totalPrice = 0;
-		List<Cart> carts = cartDao.findCartsByUserId(user.getUserId());
-		for(Cart cart : carts) {
-			cart.getProduct().setPrice((cart.getProduct().getPrice()) * (cart.getProductQuantity()));
-			totalPrice += ((cart.getProduct().getPrice()));
-		}
-		model.addAttribute("carts", carts);
-		model.addAttribute("totalPrice", totalPrice);
-		return "drawer_cart";
+	    Map<String, Object> response = new HashMap<>();
+
+	    // 先找到 user 登入者
+	    User user = (User) session.getAttribute("user");
+
+	    if (user != null) {
+	        List<Cart> carts = cartDao.findCartsByUserId(user.getUserId());
+
+	        Integer totalPrice = 0;
+	        for (Cart cart : carts) {
+	            cart.getProduct().setPrice(cart.getProduct().getPrice() * cart.getProductQuantity());
+	            totalPrice += cart.getProduct().getPrice();
+	        }
+
+	        response.put("carts", carts);
+	        response.put("totalPrice", totalPrice);
+	    } else {
+	        response.put("error", "User not logged in");
+	    }
+
+	    try {
+	        // 將 response 轉換為 JSON 字符串
+	        ObjectMapper objectMapper = new ObjectMapper();
+	        String jsonResponse = objectMapper.writeValueAsString(response);
+
+	        return jsonResponse;
+	    } catch (JsonProcessingException e) {
+	        // 處理轉換失敗的例外
+	        e.printStackTrace();
+	        return "{\"error\": \"Error converting to JSON\"}";
+	    }
 	}
 	
 	@PostMapping("/updateCartByPost")
