687ba94719b0c1858c3ee01221a75ca4d1768163
chris <chris@localhost>
2024-01-25 18:24:59 +0800

    merge stone 分支的程式

--- ScanGo/src/main/java/com/example/controller/cart/ProductController.java
+++ ScanGo/src/main/java/com/example/controller/cart/ProductController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,7 +26,6 @@ import com.example.entity.Product;
 
 @Controller
 @RequestMapping
-@CrossOrigin("*")
 public class ProductController {
 
 	@Autowired
