7bde044834f472582dccfbfe1ff00d098da4a6b5
chris <chris@localhost>
2024-01-25 18:28:19 +0800

    merge chris 分支

--- ScanGo/src/main/java/com/example/controller/user/IndexController.java
+++ ScanGo/src/main/java/com/example/controller/user/IndexController.java
@@ -1,31 +1,18 @@
 package com.example.controller.user;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.example.dao.CouponDAO;
-import com.example.entity.Coupon;
 
 @Controller
 @RequestMapping("/index")
 public class IndexController {
-	
-	@Autowired
-	@Qualifier("couponDaoImpl")
-	private CouponDAO couponDao;
 
 	// 登入首頁
 	@GetMapping
-	public String index(Model model) {
-		List<Coupon> coupon = couponDao.findAllCoupons();
+	public String index() {
 		
-		model.addAttribute("coupon", coupon);
+		
 		
 		return "index";
 	}
