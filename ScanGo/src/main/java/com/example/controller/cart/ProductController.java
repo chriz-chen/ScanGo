c708b679756a1717a7f8705a736707f64115d916
chris <chris@10.0.0.199>
2024-01-28 20:44:15 +0800

    chris 提交 css修改

--- ScanGo/src/main/java/com/example/controller/cart/ProductController.java
+++ ScanGo/src/main/java/com/example/controller/cart/ProductController.java
@@ -114,7 +114,7 @@ public class ProductController {
 	 * @throws IOException
 	 */
 	@PostMapping("/addProduct")
-    public String addProduct(@ModelAttribute Product product, Model model) throws WriterException, IOException {
+    public String addProduct(@ModelAttribute Product product, Model model, HttpServletResponse resp) throws WriterException, IOException {
         
         productDao.addProduct(product);
         
@@ -128,6 +128,14 @@ public class ProductController {
 		String str = "/ScanGo/mvc/product/"+productId;
 		qrCode.generateQRcode(str, sorucrePath.toAbsolutePath().toString(), "UTF-8", 200, 200);
 		model.addAttribute("product", product);
+		
+		// QRCode圖片下載
+		resp.setContentType("APPLICATION/OCTET-STREAM");
+		resp.setHeader("Content-Disposition", "attachment; filename=\"" + filenameEncode(filename) + "\"");
+		try (OutputStream output = resp.getOutputStream()){
+			Files.copy(sorucrePath, output);
+		}
+		
         return "add_product_result";
     }
 	
