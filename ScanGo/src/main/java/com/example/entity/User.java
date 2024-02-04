3205370b01a206ae89efca65b88468039154c8e8
chris <chris@localhost>
2024-02-04 15:09:31 +0800

    docs: 修正 User entity 註解

--- ScanGo/src/main/java/com/example/entity/User.java
+++ ScanGo/src/main/java/com/example/entity/User.java
@@ -13,19 +13,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-/**
-使用者
-level: 1(一般會員-進行團購), 2(後臺維運人員-進行團購+上架商品)
-+--------+----------+----------+-------+----------+--------+
-| userId | username | password | level | authType | authId |
-+--------+----------+----------+-------+----------+--------+
-|  101   | user123  | pass123  |   1   |          |        | 
-|  102   | user456  | pass456  |   2   |          |        | 
-|  103   | user789  | pass789  |   1   |          |        | 
-|  104   | gitUser  | None     |   1   |  github  | 123456 |
-+--------+----------+----------+-------+----------+--------+
-*/
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -34,6 +21,7 @@ public class User {
 	private Integer userId;
 	private String username; // 使用者帳號
 	private String password;
+	private Integer levelId; // 使用者權限（1: 一般會員-進行購物, 2: 後臺維運人員-上架商品）
 	private String email;
 	private String phone;
 	
@@ -42,20 +30,13 @@ public class User {
 	@Temporal(TemporalType.DATE)            // 指定實體屬性與資料庫中的日期類型對應(不包括時間部分)
 	private Date birthday;
 	
-	private Integer levelId; // 使用者權限
-	
 	private String authType; // 授權來源
 	private String authId; // 授權Id
-	
 	
 	@JsonFormat(pattern = "yyyy-MM-dd")     
 	@DateTimeFormat(pattern = "yyyy-MM-dd") 
 	@Temporal(TemporalType.DATE)
 	private Date createDate;
-	//private String avator; // 使用者大頭照
-	
-	// 服務列表
-	// private List<Service> services;
 	
 	private Level level;
 }
